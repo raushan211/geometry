82c77469bf96e29dd5449eba8290814e371eac38
raushan211 <raazcrzy90@gmail.com>
2022-06-09 14:11:14 +0530

    geometry

--- geometry.go
+++ geometry.go
@@ -8,50 +8,49 @@ func main() {
 	var number int
 	fmt.Scanln(&number)
 	fmt.Println("enter operation: ")
-	// area or perimeter
-	var operation string
+	// 1 for area, 2 for perimeter
+	var operation int
 	fmt.Scanln(&operation)
 
-	var result int
-	if number == "1" && operation == "area" {
-		fmt.Println("enter lenght: ")
-		var lenght int
-		fmt.Scanln(&lenght)
+	var result float32
+	if number == 1 && operation == 1 {
+		fmt.Println("enter length: ")
+		var length int
+		fmt.Scanln(&length)
 		fmt.Println("enter breadth: ")
 		var breadth int
 		fmt.Scanln(&breadth)
 		result = length * breadth
-	}
-	else if number == "1" && operation == "perimeter" {
-		fmt.Println("enter lenght: ")
-		var lenght int
-		fmt.Scanln(&lenght)
+	} else if number == 1 && operation == 2 {
+		fmt.Println("enter length: ")
+		var length int
+		fmt.Scanln(&length)
 		fmt.Println("enter breadth: ")
 		var breadth int
 		fmt.Scanln(&breadth)
-		result = 2 * (lenght + breadth)
-	} else if number == "2" && operatin == "area" {
+		result = 2 * (length + breadth)
+	} else if number == 2 && operation == 1 {
 		fmt.Println("enter radius: ")
-		var radius int
+		var radius float32
 		fmt.Scanln(&radius)
 		result = 3.14 * radius * radius
-	} else if number == "2" && operatin == "perimeter" {
+	} else if number == 2 && operation == 2 {
 		fmt.Println("enter radius: ")
-		var radius int
+		var radius float32
 		fmt.Scanln(&radius)
-		result = 2 * 3.14 * radius 
-    } else if number == "3" && operation == "area" {
+		result = 2 * 3.14 * radius
+	} else if number == 3 && operation == 1 {
 		fmt.Println("enter side: ")
-		var side int
+		var side float32
 		fmt.Scanln(&side)
 		result = side * side
-	} else if number == "3" && opearation == "perimeter" {
+	} else if number == 3 && opearation == 2 {
 		fmt.Println("enter side: ")
-		var side int
+		var side float32
 		fmt.Scanln(&side)
 		result = 4 * side
 	} else {
 		fmt.Println("wrong input")
 	}
-
+	fmt.Println(result)
 }
